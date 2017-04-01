65d9bc7dd9da72f854f456f232883db1ee1c1ad4
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-04-01 21:42:29 +0100

    Updates

--- StandardControls/WxTextCtrl1/src/WxTextCtrl1Frame.cpp
+++ StandardControls/WxTextCtrl1/src/WxTextCtrl1Frame.cpp
@@ -1,23 +1,23 @@
 /*
-	Copyright (c) 2015 Xavier Leclercq
+    Copyright (c) 2015 Xavier Leclercq
 
-	Permission is hereby granted, free of charge, to any person obtaining a
-	copy of this software and associated documentation files (the "Software"),
-	to deal in the Software without restriction, including without limitation
-	the rights to use, copy, modify, merge, publish, distribute, sublicense,
-	and/or sell copies of the Software, and to permit persons to whom the
-	Software is furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
 
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-	IN THE SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
 */
 
 #include "WxTextCtrl1Frame.h"
@@ -26,24 +26,24 @@
 #include <wx/sizer.h>
 
 WxTextCtrl1Frame::WxTextCtrl1Frame(const wxString& title)
-	: wxFrame(NULL, wxID_ANY, title)
+    : wxFrame(NULL, wxID_ANY, title)
 {
-	// Create a top-level panel to hold all the contents of the frame
-	wxPanel* panel = new wxPanel(this, wxID_ANY);
+    // Create a top-level panel to hold all the contents of the frame
+    wxPanel* panel = new wxPanel(this, wxID_ANY);
 
-	// Create the wxTextCtrl widget
-	wxTextCtrl* textCtrl = new wxTextCtrl(panel, wxID_ANY,
-		L"The quick brown fox jumps over the lazy dog",
-		wxDefaultPosition, wxSize(350, 50));
+    // Create the wxTextCtrl widget
+    wxTextCtrl* textCtrl = new wxTextCtrl(panel, wxID_ANY,
+        L"The quick brown fox jumps over the lazy dog",
+        wxDefaultPosition, wxSize(350, 50));
 
-	// Set up the sizer for the panel
-	wxBoxSizer* panelSizer = new wxBoxSizer(wxHORIZONTAL);
-	panelSizer->Add(textCtrl, 1, wxEXPAND);
-	panel->SetSizer(panelSizer);
+    // Set up the sizer for the panel
+    wxBoxSizer* panelSizer = new wxBoxSizer(wxHORIZONTAL);
+    panelSizer->Add(textCtrl, 1, wxEXPAND);
+    panel->SetSizer(panelSizer);
 
-	// Set up the sizer for the frame and resize the frame
-	// according to its contents
-	wxBoxSizer* topSizer = new wxBoxSizer(wxHORIZONTAL);
-	topSizer->Add(panel, 1, wxEXPAND);
-	SetSizerAndFit(topSizer);
+    // Set up the sizer for the frame and resize the frame
+    // according to its contents
+    wxBoxSizer* topSizer = new wxBoxSizer(wxHORIZONTAL);
+    topSizer->Add(panel, 1, wxEXPAND);
+    SetSizerAndFit(topSizer);
 }
