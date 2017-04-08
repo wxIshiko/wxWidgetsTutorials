6d4621ddc410e8502f0b55c3b505c2e4667aef81
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-04-08 16:51:09 +0100

    Updates

--- wxStyledTextCtrl/WxStyledTextCtrl1/src/WxStyledTextCtrl1Frame.cpp
+++ wxStyledTextCtrl/WxStyledTextCtrl1/src/WxStyledTextCtrl1Frame.cpp
@@ -1,23 +1,23 @@
 /*
-	Copyright (c) 2015 Xavier Leclercq
+    Copyright (c) 2015 Xavier Leclercq
 
-	Permission is hereby granted, free of charge, to any person obtaining a
-	copy of this software and associated documentation files (the "Software"),
-	to deal in the Software without restriction, including without limitation
-	the rights to use, copy, modify, merge, publish, distribute, sublicense,
-	and/or sell copies of the Software, and to permit persons to whom the
-	Software is furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
 
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-	IN THE SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
 */
 
 #include "WxStyledTextCtrl1Frame.h"
@@ -26,31 +26,31 @@
 #include <wx/sizer.h>
 
 WxStyledTextCtrl1Frame::WxStyledTextCtrl1Frame(const wxString& title)
-	: wxFrame(NULL, wxID_ANY, title)
+    : wxFrame(NULL, wxID_ANY, title)
 {
-	// Create a top-level panel to hold all the contents of the frame
-	wxPanel* panel = new wxPanel(this, wxID_ANY);
+    // Create a top-level panel to hold all the contents of the frame
+    wxPanel* panel = new wxPanel(this, wxID_ANY);
 
-	// Create a wxStyledTextCtrl control
-	wxStyledTextCtrl* styledTextCtrl = new wxStyledTextCtrl(panel, wxID_ANY,
-		wxDefaultPosition, wxSize(350, 200));
+    // Create a wxStyledTextCtrl control
+    wxStyledTextCtrl* styledTextCtrl = new wxStyledTextCtrl(panel, wxID_ANY,
+        wxDefaultPosition, wxSize(350, 200));
 
-	styledTextCtrl->AddText("// A simple program\n\n");
-	styledTextCtrl->AddText("#include <stdio.h>\n\n");
-	styledTextCtrl->AddText("int main(int argc, char* argv[])\n");
-	styledTextCtrl->AddText("{\n");
-	styledTextCtrl->AddText("    printf(\"Hello World!\");\n");
-	styledTextCtrl->AddText("    return 0;\n");
-	styledTextCtrl->AddText("}\n");
+    styledTextCtrl->AddText("// A simple program\n\n");
+    styledTextCtrl->AddText("#include <stdio.h>\n\n");
+    styledTextCtrl->AddText("int main(int argc, char* argv[])\n");
+    styledTextCtrl->AddText("{\n");
+    styledTextCtrl->AddText("    printf(\"Hello World!\");\n");
+    styledTextCtrl->AddText("    return 0;\n");
+    styledTextCtrl->AddText("}\n");
 
-	// Set up the sizer for the panel
-	wxBoxSizer* panelSizer = new wxBoxSizer(wxHORIZONTAL);
-	panelSizer->Add(styledTextCtrl, 1, wxEXPAND);
-	panel->SetSizer(panelSizer);
+    // Set up the sizer for the panel
+    wxBoxSizer* panelSizer = new wxBoxSizer(wxHORIZONTAL);
+    panelSizer->Add(styledTextCtrl, 1, wxEXPAND);
+    panel->SetSizer(panelSizer);
 
-	// Set up the sizer for the frame and resize the frame
-	// according to its contents
-	wxBoxSizer* topSizer = new wxBoxSizer(wxHORIZONTAL);
-	topSizer->Add(panel, 1, wxEXPAND);
-	SetSizerAndFit(topSizer);
+    // Set up the sizer for the frame and resize the frame
+    // according to its contents
+    wxBoxSizer* topSizer = new wxBoxSizer(wxHORIZONTAL);
+    topSizer->Add(panel, 1, wxEXPAND);
+    SetSizerAndFit(topSizer);
 }
