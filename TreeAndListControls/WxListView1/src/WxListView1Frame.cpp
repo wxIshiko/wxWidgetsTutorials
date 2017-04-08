9d720fd29da7415d3dd02e5848ea246b0517049b
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-04-08 19:41:43 +0100

    Added WxListView1 example

---
+++ TreeAndListControls/WxListView1/src/WxListView1Frame.cpp
@@ -0,0 +1,56 @@
+/*
+	Copyright (c) 2015 Xavier Leclercq
+
+	Permission is hereby granted, free of charge, to any person obtaining a
+	copy of this software and associated documentation files (the "Software"),
+	to deal in the Software without restriction, including without limitation
+	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+	and/or sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+	IN THE SOFTWARE.
+*/
+
+#include "WxListView1Frame.h"
+#include <wx/panel.h>
+#include <wx/listctrl.h>
+#include <wx/sizer.h>
+
+WxListView1Frame::WxListView1Frame(const wxString& title)
+	: wxFrame(NULL, wxID_ANY, title)
+{
+	// Create a top-level panel to hold all the contents of the frame
+	wxPanel* panel = new wxPanel(this, wxID_ANY);
+
+	// Create a wxListView control with one column.
+	// The default style is wxLC_REPORT so columns need
+	// to be added explicitly.
+	wxListView* listView = new wxListView(panel, wxID_ANY, 
+		wxDefaultPosition, wxSize(250, 200));
+	listView->AppendColumn("Column 1");
+	
+	// Add three items to the list
+	listView->InsertItem(0, "Item 1");
+	listView->InsertItem(1, "Item 2");
+	listView->InsertItem(2, "Item 3");
+
+	// Set up the sizer for the panel
+	wxBoxSizer* panelSizer = new wxBoxSizer(wxHORIZONTAL);
+	panelSizer->Add(listView, 1, wxEXPAND);
+	panel->SetSizer(panelSizer);
+
+	// Set up the sizer for the frame and resize the frame
+	// according to its contents
+	wxBoxSizer* topSizer = new wxBoxSizer(wxHORIZONTAL);
+	topSizer->Add(panel, 1, wxEXPAND);
+	SetSizerAndFit(topSizer);
+}
