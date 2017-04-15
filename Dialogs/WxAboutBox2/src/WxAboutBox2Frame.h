e56d63f6218606ff7c11c948d6c202c1d187c35a
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-04-15 22:12:18 +0100

    Added WxAboutBox2 example

---
+++ Dialogs/WxAboutBox2/src/WxAboutBox2Frame.h
@@ -0,0 +1,39 @@
+/*
+	Copyright (c) 2015 Xavier Leclercq
+
+	Permission is hereby granted, free of charge, to any person obtaining  a
+	copy of this software and associated documentation files (the "Software"),
+	to deal in the Software without restriction, including without limitation
+	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+	and/or sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+	IN THE SOFTWARE.
+*/
+
+#ifndef _TUTORIALS_WXWIDGETS_WXABOUTBOX2FRAME_H_
+#define _TUTORIALS_WXWIDGETS_WXABOUTBOX2FRAME_H_
+
+#include <wx/frame.h>
+
+class WxAboutBox2Frame : public wxFrame
+{
+public:
+	WxAboutBox2Frame(const wxString& title);
+
+private:
+	void OnAbout(wxCommandEvent& evt);
+
+	wxDECLARE_EVENT_TABLE();
+};
+
+#endif
